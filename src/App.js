2543d6f0614a6b0af8fa56ecbca11ee6775fa1bc
Pascual Muñoz <pascual.munoz@pccomponentes.com>
2018-08-14 23:19:41 +0200

    eslinted

--- src/App.js
+++ src/App.js
@@ -3,18 +3,16 @@ import './App.css'
 import { ajax } from 'rxjs/ajax'
 //import { map } from 'rxjs/operators';
 
-import USER_ROLES from './USER_ROLES.js'
-import logo from './logo.svg'
-import boy from './boy.svg'
-import paramita from './paramita.svg'
+// import USER_ROLES from './USER_ROLES.js'
+// import logo from './logo.svg'
+// import boy from './boy.svg'
+// import paramita from './paramita.svg'
 import nicolas from './nicolas.svg'
 
 // function App(props) {return <PutoModal content={"cosa"} /> }
 
 class App extends Component {
-    constructor(props) {
-      window.userRoles = USER_ROLES;
-      console.log(USER_ROLES)
+    constructor (props) {
         super(props)
         this.state = {
             error: null,
@@ -22,20 +20,21 @@ class App extends Component {
             cursos: [],
         }
     }
-    componentDidMount() {
+
+    componentDidMount () {
         cursos().subscribe(
             (item) => {this.setState({cursos: item.response})},
             (error) => {this.setState({error: error})},
         )
 
-
     }
-    render() {
+
+    render () {
         console.log('cursos', this.state.cursos)
         return (
             <div className="App">
                 <header className="App-header">
-                    <img src={'https://image.flaticon.com/icons/svg/55/55316.svg'} className="App-logo" alt="logo" />
+                    <img src={'https://image.flaticon.com/icons/svg/55/55316.svg'} className="App-logo" alt="logo"/>
                     <h1 className="App-title">oajax & basic component react example</h1>
                 </header>
                 <CursoGrid cursos={this.state.cursos} onClickCustomHandler={alerta}/>
@@ -51,22 +50,21 @@ const cursos = () => {
     return simpleOajax$
 }
 
-function alerta(e){console.log(e)}
+function alerta (e) {console.log(e)}
 
 class CursoGrid extends Component {
-    render(){
+    render () {
         const allElements = this.props.cursos
         //.filter( (item)=> item.hasExpired !== 'true' )
-            .map((item)=>{return <Curso key={item.id} curso={item} onClickCustomHandler={alerta}/>})
-
- 	return <div className="grid-container-cursos" > {allElements} </div>
-    }	
+            .map((item) => {return <Curso key={item.id} curso={item} onClickCustomHandler={alerta}/>})
+        return <div className="grid-container-cursos"> {allElements} </div>
+    }
 }
 
-function Curso(props){
+function Curso (props) {
     return <div className={'grid-item-cursos'} onClick={props.onClickCustomHandler}>
-       <img src={'https://image.flaticon.com/icons/svg/753/753045.svg'} className="Avatar" alt="logo" />
-        <input type="image" className={'Avatar'} src= 'https://image.flaticon.com/icons/svg/831/831147.svg' />
+        <img src={'https://image.flaticon.com/icons/svg/753/753045.svg'} className="Avatar" alt="logo"/>
+        <input type="image" className={'Avatar'} src='https://image.flaticon.com/icons/svg/831/831147.svg'/>
         <div>{props.curso.datestart}</div>
         <div>{props.curso.hasexpired}</div>
         <div>{props.curso.id}</div>
@@ -76,43 +74,43 @@ function Curso(props){
     </div>
 }
 
-function InscriptionGrid(props){
+function InscriptionGrid (props) {
     const AllElements = props.inscriptions
-        .map ( (item) => <Inscription key={item.name }name={item.name}/> )
+        .map((item) => <Inscription key={item.name} name={item.name}/>)
     return <div className="grid-container-inscritos">{AllElements}</div>
 }
 
-function iamClicked(){
-  //todo stub
-return  false;
+function iamClicked () {
+    //todo stub
+    return false
 }
 
-function Inscription(props){
-    const classNameWithJumpIfImClicked = "grid-item-inscritos".concat(  (iamClicked())?" jump":"")
+function Inscription (props) {
+    const classNameWithJumpIfImClicked = 'grid-item-inscritos'.concat((iamClicked()) ? ' jump' : '')
 
-    return <div className={ classNameWithJumpIfImClicked } onClick={alerta}>
-    {props.name}
-      <NicolasCage />
-       <PutoModal content={<div> y se le puede meter un elemento</div>}/>
+    return <div className={classNameWithJumpIfImClicked} onClick={alerta}>
+        {props.name}
+        <NicolasCage/>
+        <PutoModal content={<div> y se le puede meter un elemento</div>}/>
     </div>
 }
 
-function NicolasCage() {
-       return <img src={nicolas} className="Avatar" alt="logo" />
+function NicolasCage () {
+    return <img src={nicolas} className="Avatar" alt="logo"/>
 }
 
-function PutoModal(props){
-  const style  = <style type="text/css"> 
-  </style>;
+function PutoModal (props) {
+    const style = <style type="text/css">
+    </style>
 
-  const modalHtml =  <div className="modal">
-    <div className="modal-content">
-    <div className="modal-close">X</div>   
-      contenido: {props.content}
+    const modalHtml = <div className="modal">
+        <div className="modal-content">
+            <div className="modal-close">X</div>
+            contenido: {props.content}
+        </div>
     </div>
-  </div>
 
-    return <div className="superContainer">{style}{modalHtml}</div>;
+    return <div className="superContainer">{style}{modalHtml}</div>
 }
 
 export default App
